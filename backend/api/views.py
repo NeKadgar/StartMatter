5261195435b84df6789e495942e2f7796f8724af
NeKadgar <37880360+NeKadgar@users.noreply.github.com>
2020-02-07 16:21:05 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -16,35 +16,7 @@ def circle_distance(lati_1, lati_2, long_1, long_2):
     x = (m.sin(lati_1)*m.sin(lati_2))+(m.cos(lati_1)*m.cos(lati_2)*m.cos(d_long))
     distance = m.acos(x) * 6371
     return distance
-'''
-@api_view(['POST'])
-def CustomersAround(request):
-    try:
-        file = request.data['file']
-        Office_lati = float(request.data['latitude'])
-        Office_long = float(request.data['longitude'])
-        customers_list = file.read().decode('utf8').replace("'", '"')
-        customers_list = '['+customers_list.replace('\n', ',')+']'
-        c_around = []
-        for i in json.loads(customers_list):
-            C_lati = float(i['latitude'])
-            C_long = float(i['longitude'])
-            distance = circle_distance(Office_lati, C_lati, Office_long, C_long)
-            if distance <=100:
-                customer = {'user_id':i['user_id'], 'name':i['name']}
-                c_around.append(customer)
 
-        for i in range(len(c_around)-1):
-            for j in range(len(c_around)-i-1):
-                if c_around[j]['user_id'] > c_around[j+1]['user_id']:
-                    c_around[j], c_around[j+1] = c_around[j+1], c_around[j]
-
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'POST':
-        return JsonResponse({'data':c_around})
-'''
 
 @api_view(['POST'])
 def CustomersAround(request):
