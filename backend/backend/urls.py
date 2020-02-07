89042d5c5a491e83daa5ab15ec26bcf384cebeef
NeKadgar <37880360+NeKadgar@users.noreply.github.com>
2020-02-07 16:27:23 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-router = DefaultRouter()
+
 
 urlpatterns = [
     path('v1/api/', include('api.urls')),
